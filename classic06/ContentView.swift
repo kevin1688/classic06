99650d8c085ef7029406bfc4c67767fd44b77d0e
kb18k <jackexi@gmail.com>
2021-09-24 21:07:21 +0800

    Initial Commit

---
+++ classic06/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  classic06
+//
+//  Created by kai wen chen on 2021/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ classic06/classic06App.swift
@@ -0,0 +1,17 @@
+//
+//  classic06App.swift
+//  classic06
+//
+//  Created by kai wen chen on 2021/9/24.
+//
+
+import SwiftUI
+
+@main
+struct classic06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
